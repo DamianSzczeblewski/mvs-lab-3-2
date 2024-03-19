def93ed526c767dd4100528300f2c9a789ecd4c7
DamianSzczeblewski <163163276+DamianSzczeblewski@users.noreply.github.com>
2024-03-19 20:00:15 +0100

    home.js

--- zad4/views/home.js
+++ zad4/views/home.js
@@ -1,6 +1,6 @@
-function renderPage() {
+function renderPage(response) {
     response.setHeader("Content-Type", "text/html");
-    response.write("<html>");
+    response.write(`<html lang="pl">`);
     response.write(`
     <head>
         <meta charset="UFT-8">
@@ -10,18 +10,18 @@ function renderPage() {
     `);
     response.write(`
     <body>
-        <form action="/message" method="POST">
-            <input type="text" name="name"/>
-            <input type="text" name="lastname"/>
-            <input type="text" name="age"/>
-            <select name="gender">
-                <option>male</option>
-                <option>female</option>
-                <option>other</option>
-            </select>
-            <input type="number" name="code"/>
-            <input type="text" name="studyField"/>
-            <button>Send</button>
+        <form action="/student" method="POST">
+        <input type="text" name="name"/>
+        <input type="text" name="lastname"/>
+        <input type="number" name="age"/>
+        <select name="gender">
+            <option>male</option>
+            <option>female</option>
+            <option>other</option>
+        </select>
+        <input type="number" name="code"/>
+        <input type="text" name="studyField"/>
+        <button>Send</button>
         </form>
     </body>
     `);
