7a72827890f590887f92e8d83439eeda224c341d
DamianSzczeblewski <163163276+DamianSzczeblewski@users.noreply.github.com>
2024-03-19 19:59:16 +0100

    app.js

--- zad4/app.js
+++ zad4/app.js
@@ -1,4 +1,8 @@
 const http = require("http");
+const fs = require("fs");
+
+const home = require("./views/home");
+const student = require("./views/student");
 
 const PORT = 3000;
 
@@ -8,20 +12,29 @@ function requestListener(request, response) {
     const { url, method } = request;
 
     if (url === "/") {
-        views/home.renderPage();
+        home.renderPage(response);
     }
 
-    else if (url === "/student") {
-        views/student.renderPage();
+    else if (url === "/student" && method === "POST") {
+        const body = [];
+        request.on("data", (chunk) => {
+            body.push(chunk);
+        });
+        return request.on("end", () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split("=")[1].replace("+", " ");
+            fs.writeFileSync("message.txt", message);
+            student.renderPage(response);
+        });
     }
 
     else {
+        response.statuscode = 404;
         response.setHeader("Content-Type", "text/html");
         response.write("<html>");
         response.write("<head><title>404</title></head>");
-        response.write(`<body>"404 Not Found"</body>`);
+        response.write("<body>404 Not Found</body>");
         response.write("</html>");
-        response.end();
     }
 
     console.log(`Server is running on ${PORT}`);
