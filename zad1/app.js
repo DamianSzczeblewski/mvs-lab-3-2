4db65eeea2618acc87a3dcaf1906fbb245038287
DamianSzczeblewski <163163276+DamianSzczeblewski@users.noreply.github.com>
2024-04-08 15:23:25 +0200

    Update app.js

--- zad1/app.js
+++ zad1/app.js
@@ -8,17 +8,23 @@ const PORT = 3000;
 const server = http.createServer(requestListener);
 
 function requestListener(request, response) {
-    const { url, method } = request;
+    const { url } = request;
 
     if (url === "/") {
-        home.renderPage(response);
+        response.setHeader("Content-Type", "text/html");
+        response.write(home.renderPage());
+        response.end();
+        return;
     }
 
     if (url === "/student") {
-        student.renderPage(response);
+        response.setHeader("Content-Type", "text/html");
+        response.write(student.renderPage());
+        response.end();
+        return;
     }
+};
 
-    console.log(`Server is running on ${PORT}`);
-}
-
-server.listen(PORT);
+server.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
