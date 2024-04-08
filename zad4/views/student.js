5d010dfc543d55484dfc84a0bdc35938e71fa871
DamianSzczeblewski <163163276+DamianSzczeblewski@users.noreply.github.com>
2024-04-08 15:38:03 +0200

    Update student.js

--- zad4/views/student.js
+++ zad4/views/student.js
@@ -1,27 +1,24 @@
-function renderPage(response) {
-    response.setHeader("Content-Type", "text/html");
-    response.write(`<html lang="pl">`);
-    response.write(`
-    <head>
-        <meta charset="UFT-8">
-        <meta name="viewport" content="width, initial-scale=1.0">
-        <title>student</title>
-    </head>
-    `);
-    response.write(`
-    <body>
-        <h1>Student profile</h1>
-        <p>Code: </p>
-        <p>Name: </p>
-        <p>Last name: </p>
-        <p>Gender: </p>
-        <p>Age: </p>
-        <p>Study field: </p>
-    </body>
-    `);
-    response.write("</html>");
-    return response.end();
-}
+const renderPage = ({ name, lastname, age, gender, code, studyField }) => {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>STUDENT</title>
+        </head>
+        <body>
+            <h1>Student profile</h1>
+            <p>Student code: ${code ?? ""}</p>
+            <p>Name: ${name ?? ""}</p>
+            <p>Lastname: ${lastname ?? ""}</p>
+            <p>Gender: ${gender ?? ""}</p>
+            <p>Age: ${age ?? ""}</p>
+            <p>Study field: ${studyField ?? ""}</p>
+        </body>
+    </html>
+    `;
+};
 
 module.exports = {
     renderPage,
