6319496bf55b4abb30392dd9b3f114588e2dae34
DamianSzczeblewski <163163276+DamianSzczeblewski@users.noreply.github.com>
2024-04-08 15:29:23 +0200

    Update app.js

--- zad3/app.js
+++ zad3/app.js
@@ -11,23 +11,32 @@ function requestListener(request, response) {
     const { url, method } = request;
 
     if (url === "/") {
-        home.renderPage(response);
-    }
-
-    else if (url === "/student" && method === "POST") {
-        student.renderPage(response);
-    }
-
-    else {
-        response.statuscode = 404;
         response.setHeader("Content-Type", "text/html");
-        response.write("<html>");
-        response.write("<head><title>404</title></head>");
-        response.write("<body>404 Not Found</body>");
-        response.write("</html>");
+        response.write(routes.handleHome());
+        response.end();
+        return;
     }
 
-    console.log(`Server is running on ${PORT}`);
+    if (url === "/student" && method === "POST") {
+        response.statusCode = 302;
+        response.setHeader("Location", "/");
+        response.end();
+        return;
+    }
+
+    if (url === "/student") {
+        response.setHeader("Content-Type", "text/html");
+        response.write(routes.handleStudent());
+        response.end();
+        return;
+    }
+
+    response.statusCode = 404;
+    response.setHeader("Content-Type", "text/html");
+    response.write("404 Not Found");
+    response.end();
 }
 
-server.listen(PORT);
+server.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
