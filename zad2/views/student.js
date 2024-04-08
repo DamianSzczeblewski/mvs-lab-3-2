268ab9efc1e1c75b9eb4bf28d8903f28a7d49015
DamianSzczeblewski <163163276+DamianSzczeblewski@users.noreply.github.com>
2024-04-08 15:27:11 +0200

    Update student.js

--- zad2/views/student.js
+++ zad2/views/student.js
@@ -1,21 +1,18 @@
-function renderPage(response) {
-    response.setHeader("Content-Type", "text/html");
-    response.write(`<html lang="pl">`);
-    response.write(`
-    <head>
-        <meta charset="UFT-8">
-        <meta name="viewport" content="width, initial-scale=1.0">
-        <title>student</title>
-    </head>
-    `);
-    response.write(`
-    <body>
-        <div>student</div>
-    </body>
-    `);
-    response.write("</html>");
-    return response.end();
-}
+const renderPage = () => {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>STUDENT</title>
+        </head>
+        <body>
+            <div>STUDENT</div>
+        </body>
+    </html>
+    `;
+};
 
 module.exports = {
     renderPage,
